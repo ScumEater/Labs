c6be37589c1ec6cde13f5aebb2aea30de377139d
ScumEater <nikita2013282@gmail.com>
2019-10-08 19:46:03 +0000

    добавление лабы на питоне

---
+++ lab1/lab.py
@@ -0,0 +1,14 @@
+from math import *
+x = int (input("Введите x: "))
+a = int (input("Введите а: "))
+g=5*(-10*a*a+27*a*x+28*x*x)/(5*a*a-9*a*x+4*x*x)
+print(g)
+x = int (input("Введите x: "))
+a = int (input("Введите а: "))
+f=sin(28*a*a-57*a*x+14*x*x)
+print(f)
+x = int (input("Введите x: "))
+a = int (input("Введите а: "))
+y=log(-27*a*a+24*a*x+35*x*x+1)
+print(y)
+
